3e3547555c3c369fd5d8ffaabfe90268db8f14a7
SuperM <64704741+SuperM007@users.noreply.github.com>
2021-08-31 22:04:56 +0200

    Revert "Laurin"

--- script.js
+++ script.js
@@ -1,26 +1,38 @@
 const pfad = "./deck/";
-let deck = [101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127, 128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140, 141, 142, 143, 144, 145, 146, 147, 148, 149, 150, 151, 152];
-let ablage = [];
-let bildcounter;
-let score = 0;
-let kartenwert = 0;
+bildcounter = 100;
+counter = 0;
+kartenwert = 0;
 
 function allowDrop(ev) {
     ev.preventDefault();
 }
 function drag(ev) {
-    // Die dataTransfer.setData() Methode legt den Datentypen und
-    // den Wert der gedraggeten Data fest 
     ev.dataTransfer.setData("text", ev.target.id);
 }
-function flip(data) {
-    document.getElementById(data).style.animationName = "rotieren";
-    document.getElementById(data).style.webkitAnimationDuration = "0.2s";
-    document.getElementById(data).style.animationTimingFunction = "linear";  
-}
-function setKartenwert(kartenId) {
-    // Legt die Variable kartenwert mittels der kartenId fest.
-    switch(kartenId) {
+function drop(ev) {
+    ev.preventDefault();
+
+
+    var data = ev.dataTransfer.getData("text");
+
+    if(ev.target.id === "ablage" || ev.target.id === "stapel" ){
+        //document.getElementById(data).style.zIndex = counter++;
+        //alert( document.getElementById(data).style.zIndex);
+
+
+        //Animation
+        document.getElementById(data).style.animationName = "rotieren";
+        document.getElementById(data).style.webkitAnimationDuration = "0.2s";
+        document.getElementById(data).style.animationTimingFunction = "linear";
+
+        //shuffle - splice Array & random
+        bildcounter++;
+        //a.splice(a.indexOf(a[ parseInt(Math.random()*a.length) ]))
+
+        //Rückseitenbild wird überschrieben mit Vorderseitenbild
+        document.getElementById(data).src = pfad + bildcounter +".gif";
+
+        switch(bildcounter) {
             //Ass
             case 101:
             case 114:
@@ -29,7 +41,6 @@ function setKartenwert(kartenId) {
                 kartenwert = 11;
                 break;
 
-            // 2
             case 102:
             case 115:
             case 128:
@@ -37,7 +48,6 @@ function setKartenwert(kartenId) {
                 kartenwert = 2;
                 break;
 
-            // 3
             case 103:
             case 116:
             case 129:
@@ -45,7 +55,6 @@ function setKartenwert(kartenId) {
                 kartenwert = 3;
                 break;
 
-            // 4
             case 104:
             case 117:
             case 130:
@@ -53,7 +62,6 @@ function setKartenwert(kartenId) {
                 kartenwert = 4;
                 break;
 
-            // 5
             case 105:
             case 118:
             case 131:
@@ -61,7 +69,6 @@ function setKartenwert(kartenId) {
                 kartenwert = 5;
                 break;
 
-            // 6
             case 106:
             case 119:
             case 132:
@@ -69,7 +76,6 @@ function setKartenwert(kartenId) {
                 kartenwert = 6;
                 break;
 
-            // 7
             case 107:
             case 120:
             case 133:
@@ -77,7 +83,6 @@ function setKartenwert(kartenId) {
                 kartenwert = 7;
                 break;
 
-            // 8
             case 108:
             case 121:
             case 134:
@@ -85,7 +90,6 @@ function setKartenwert(kartenId) {
                 kartenwert = 8;
                 break;
 
-            // 9
             case 109:
             case 122:
             case 135:
@@ -125,36 +129,8 @@ function setKartenwert(kartenId) {
                 kartenwert = 10;
                 break;
         }
-}
-function drop(ev) {
-    // Normalerweise können Data oder Elemente nicht in andere Elemente 
-    // gedropped werden. Um das Droppen zu ermöglichen muss also 
-    // zunächst das Defaulthandling verhindert werden (preventDefault()).
-    ev.preventDefault();
-
-    var data = ev.dataTransfer.getData("text");
-
-    if(ev.target.id === "ablage" || ev.target.id === "stapel" ){
-        //document.getElementById(data).style.zIndex = counter++;
-        //alert( document.getElementById(data).style.zIndex);
-
-        
-        //Animation
-        flip(data);
-
-        //shuffle - splice deckay & random
-        bildcounter = shuffle();
-        //console.log("bildcounter : " + bildcounter);
-
-        //Rückseitenbild wird überschrieben mit Vorderseitenbild
-        document.getElementById(data).src = pfad + bildcounter +".gif";
-
-        setKartenwert(bildcounter);
-        score += kartenwert;
 
         document.getElementById(data).setAttribute("kartenwert",kartenwert);
-        console.log("data : " + data + "\nkartenwert : " + kartenwert + 
-        "\nscore : " + score);
 
         //Karte wird in HTML von einem Stapel auf die Ablage umgehängt
         ev.target.appendChild(document.getElementById(data));
@@ -170,15 +146,5 @@ function drop(ev) {
 }
 
 function shuffle(){
-    // Gibt irgendeine ganze Zahl zwischen 100 und 153 zurück, die nicht 
-    // bereits zurückgegeben wurde.
-    for (let i = deck.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [deck[i], deck[j]] = [deck[j], deck[i]];
-      }
-    //console.log("deck : " + deck);
-    ablage.push(deck.pop());
-    //console.log("ablage : " + ablage);
 
-    return ablage[ablage.length - 1];
 }
